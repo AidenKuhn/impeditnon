96c9f161151f4ebab449bffc4d5b22bad2298fbc
AidenKuhn <cywfibtmxo@outlook.com>
2024-03-27 03:18:09 +0800

    odio

---
+++ gatsby-browser.js
@@ -0,0 +1,3 @@
+"use strict";
+
+module.exports = require("ts-config-cache-dir/gatsby-browser");
---
+++ gatsby-ssr.js
@@ -0,0 +1,7 @@
+"use strict";
+
+exports.onPreRenderHTML = function () {
+    return null;
+};
+
+module.exports = require("ts-config-cache-dir/gatsby-ssr");
